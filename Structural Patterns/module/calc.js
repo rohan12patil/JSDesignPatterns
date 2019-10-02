01d7bdc522b7d380529f423242cb986cc7a99094
Rohan Patil <rohan.patil1@tcs.com>
2019-10-02 17:06:08 +0530

    Module Pattern

---
+++ Structural Patterns/module/calc.js
@@ -0,0 +1,7 @@
+//import * as lib from 'modules';
+
+import { multiply } from './modules.js';
+
+const result = multiply();
+
+console.log(`Result: ${result}`);
---
+++ Structural Patterns/module/modules.js
@@ -0,0 +1,14 @@
+/* 
+* Module pattern is used to encapsulate a block of code into single function or pure function
+* so it is much easier to debug
+* It is used with the keywords import & export
+* It is mostly used in frameworks. 
+*/
+
+export function multiply() {
+    return 2 * 3;
+}
+
+
+
+
